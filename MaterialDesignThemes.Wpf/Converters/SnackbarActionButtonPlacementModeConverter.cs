ed107964217ca5de3abdc74018a0d0cf5d94ad2e
Jan Ivar Z. Carlsen <jizc@outlook.com>
2020-05-06 12:47:04 +0200

    Merge fixes

--- MaterialDesignThemes.Wpf/Converters/SnackbarActionButtonPlacementModeConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/SnackbarActionButtonPlacementModeConverter.cs
@@ -12,14 +12,19 @@ namespace MaterialDesignThemes.Wpf.Converters
             var mode = (SnackbarActionButtonPlacementMode) values[0];
             var inlineMaxHeight = (double) values[1];
             var actualHeight = (double) values[2];
-            return mode switch
+            switch (mode)
             {
-                SnackbarActionButtonPlacementMode.Auto when actualHeight <= inlineMaxHeight => Dock.Right,
-                SnackbarActionButtonPlacementMode.Auto => Dock.Bottom,
-                SnackbarActionButtonPlacementMode.Inline => Dock.Right,
-                SnackbarActionButtonPlacementMode.SeparateLine => Dock.Bottom,
-                _ => Dock.Right
-            };
+                case SnackbarActionButtonPlacementMode.Auto when actualHeight <= inlineMaxHeight:
+                    return Dock.Right;
+                case SnackbarActionButtonPlacementMode.Auto:
+                    return Dock.Bottom;
+                case SnackbarActionButtonPlacementMode.Inline:
+                    return Dock.Right;
+                case SnackbarActionButtonPlacementMode.SeparateLine:
+                    return Dock.Bottom;
+                default:
+                    return Dock.Right;
+            }
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new InvalidOperationException();
