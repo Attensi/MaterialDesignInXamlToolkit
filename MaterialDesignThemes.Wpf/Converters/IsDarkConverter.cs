826edb8713901aa2c7ba6ba94d4e4713eacefab6
Jan Ivar Z. Carlsen <jizc@outlook.com>
2020-05-25 10:55:55 +0200

    Merge fixes

--- MaterialDesignThemes.Wpf/Converters/IsDarkConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/IsDarkConverter.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
-using MaterialDesignColors.ColorManipulation;
 
 namespace MaterialDesignThemes.Wpf.Converters
 {
@@ -10,19 +9,42 @@ namespace MaterialDesignThemes.Wpf.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value switch
+            Color? color = null;
+            switch (value)
             {
-                SolidColorBrush brush => brush.Color,
-                Color c => c,
-                _ => (Color?)null
-            } is Color color
-                ? color.ContrastingForegroundColor() == Colors.White
+                case SolidColorBrush brush:
+                    color = brush.Color;
+                    break;
+                case Color c:
+                    color = c;
+                    break;
+            }
+
+            return color.HasValue
+                ? Color.Equals(ContrastingForegroundColor(color.Value), Colors.White)
                 : Binding.DoNothing;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private static Color ContrastingForegroundColor(Color color)
+        {
+            double rgb_srgb(double d)
+            {
+                d = d / 255.0;
+                return (d > 0.03928)
+                    ? d = Math.Pow((d + 0.055) / 1.055, 2.4)
+                    : d = d / 12.92;
+            }
+            var r = rgb_srgb(color.R);
+            var g = rgb_srgb(color.G);
+            var b = rgb_srgb(color.B);
+
+            var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            return luminance > 0.179 ? Colors.Black : Colors.White;
         }
     }
 }
---
+++ MaterialDesignThemes.Wpf/ThemeAssist.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf
+{
+    internal static class ThemeAssist
+    {
+        internal static Brush GetTriggerColor(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(TriggerBrushProperty);
+        }
+
+        internal static void SetTriggerBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(TriggerBrushProperty, value);
+        }
+
+        internal static readonly DependencyProperty TriggerBrushProperty =
+            DependencyProperty.RegisterAttached("TriggerBrush", typeof(Brush), typeof(ThemeAssist), new PropertyMetadata(null));
+    }
+}
